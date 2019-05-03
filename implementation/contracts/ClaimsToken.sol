6cc392fac3f4e6c64774c59747f97dc5eb933c11
jo-es <escherich.johannes+github@gmail.com>
2019-05-03 15:06:01 +0200

    Update terminology and docstrings

--- implementation/contracts/ClaimsToken.sol
+++ implementation/contracts/ClaimsToken.sol
@@ -8,43 +8,58 @@ import "./math/SafeMathInt.sol";
 import "./IClaimsToken.sol";
 
 
-/// @title Claims Token
-/// @author Johannes Escherich
-/// @author Johannes Pfeffer
-/// @dev A  mintable token that can represent claims on cash flow of arbitrary assets such as dividends, loan repayments, 
-///   fee or revenue shares among large numbers of token holders. Anyone can deposit funds, token holders can withdraw their claims.
-///   Supports funds paid in Ether or an ERC20 token. Extensible to support standards like ERC777 or ERC223.
-///   This file implements the base accounting. Extensions implement Ether or token specific functionality.
-///   Based on EIP1726 and EIP1843. Payments accounting based on the implementation of @roger-wu and foundational work of @arachnid
-
+/** 
+ * @title Claims Token
+ * @author Johannes Escherich
+ * @author Johannes Pfeffer
+ * @dev A  mintable token that can represent claims on cash flow of arbitrary assets such as dividends, loan repayments, 
+ *   fee or revenue shares among large numbers of token holders. Anyone can deposit funds, token holders can withdraw their claims.
+ * Supports funds paid in Ether or an ERC20 token. Extensible to support standards like ERC777 or ERC223.
+ * This file implements the base accounting. Extensions implement Ether or token specific functionality.
+ * Based on EIP1726 and EIP1843. Payments accounting based on the implementation of roger-wu and foundational work of arachnid
+ */
 contract ClaimsToken is IClaimsToken, ERC20Mintable {
 
 	using SafeMath for uint256;
   using SafeMathUint for uint256;
   using SafeMathInt for int256;
 
-	uint256 constant internal magnitude = 2**128; // optimize, see https://github.com/ethereum/EIPs/issues/1726#issuecomment-472352728
-	uint256 internal magnifiedFundsPerShare; // use points terminology
+	uint256 constant internal pointsMultiplier = 2**128; // optimize, see https://github.com/ethereum/EIPs/issues/1726#issuecomment-472352728
+	uint256 internal pointsPerShare;
 
-	mapping(address => int256) internal magnifiedFundsCorrection;
+	mapping(address => int256) internal pointsCorrection;
   mapping(address => uint256) internal withdrawnFunds;
 
 
-	// distributeDividends
-  /// @notice Distributes received funds proportionally
+	/** 
+   * prev. distributeDividends
+   * @notice Distributes funds to token holders.
+   * @dev It reverts if the total supply of tokens is 0.
+   * It emits the `FundsDistributed` event if the amount of received ether is greater than 0.
+   * About undistributed funds:
+   *   In each distribution, there is a small amount of funds which does not get distributed,
+   *     which is `(msg.value * pointsMultiplier) % totalSupply()`.
+   *   With a well-chosen `pointsMultiplier`, the amount funds that are not getting distributed
+   *     in a distribution can be less than 1 (base unit).
+   *   We can actually keep track of the undistributed ether in a distribution
+   *     and try to distribute it in the next distribution ....... todo implement  
+   */
   function _distributeFunds(uint256 value) internal {
     require(totalSupply() > 0);
 
     if (value > 0) {
-      magnifiedFundsPerShare = magnifiedFundsPerShare.add(
-        value.mul(magnitude) / totalSupply()
+      pointsPerShare = pointsPerShare.add(
+        value.mul(pointsMultiplier) / totalSupply()
       );
       emit FundsDistributed(msg.sender, value);
     }
   }
 
-	// withdrawDividend
-  /// @notice Prepares funds withdrawal
+	/**
+   * prev. withdrawDividend
+   * @notice Prepares funds withdrawal
+   * @dev It emits a `FundsWithdrawn` event if the amount of withdrawn ether is greater than 0.
+   */
 	function _prepareWithdraw() internal returns (uint256) {
     uint256 _withdrawableDividend = withdrawableFundsOf(msg.sender);
 	
@@ -55,47 +70,77 @@ contract ClaimsToken is IClaimsToken, ERC20Mintable {
 		return _withdrawableDividend;
   }
 
-  // withdrawableDividendOf
-  /// @notice View the amount of funds that an address can withdraw.
+  /** 
+   * prev. withdrawableDividendOf
+   * @notice View the amount of funds that an address can withdraw.
+   * @param _owner The address of a token holder.
+   * @return The amount funds that `_owner` can withdraw.
+   */
   function withdrawableFundsOf(address _owner) public view returns(uint256) {
     return accumulativeFundsOf(_owner).sub(withdrawnFunds[_owner]);
   }
   
-  // withdrawnDividendOf
-  /// @notice View the amount of funds that an address has withdrawn.
+  /**
+   * prev. withdrawnDividendOf
+   * @notice View the amount of funds that an address has withdrawn.
+   * @param _owner The address of a token holder.
+   * @return The amount of funds that `_owner` has withdrawn.
+   */
   function withdrawnFundsOf(address _owner) public view returns(uint256) {
     return withdrawnFunds[_owner];
   }
 
-  // accumulativeDividendOf
-  /// @notice View the amount of funds that an address has earned in total.
+  /**
+   * prev. accumulativeDividendOf
+   * @notice View the amount of funds that an address has earned in total.
+   * @dev accumulativeFundsOf(_owner) = withdrawableFundsOf(_owner) + withdrawnFundsOf(_owner)
+   * = (pointsPerShare * balanceOf(_owner) + pointsCorrection[_owner]) / pointsMultiplier
+   * @param _owner The address of a token holder.
+   * @return The amount of funds that `_owner` has earned in total.
+   */
   function accumulativeFundsOf(address _owner) public view returns(uint256) {
-    return magnifiedFundsPerShare.mul(balanceOf(_owner)).toInt256Safe()
-      .add(magnifiedFundsCorrection[_owner]).toUint256Safe() / magnitude;
+    return pointsPerShare.mul(balanceOf(_owner)).toInt256Safe()
+      .add(pointsCorrection[_owner]).toUint256Safe() / pointsMultiplier;
   }
 
-  /// @dev Internal function that transfer tokens from one address to another.
+  /**
+   * @dev Internal function that transfer tokens from one address to another.
+   * Update pointsCorrection to keep funds unchanged.
+   * @param from The address to transfer from.
+   * @param to The address to transfer to.
+   * @param value The amount to be transferred.
+   */
   function _transfer(address from, address to, uint256 value) internal {
     super._transfer(from, to, value);
 
-    int256 _magCorrection = magnifiedFundsPerShare.mul(value).toInt256Safe();
-    magnifiedFundsCorrection[from] = magnifiedFundsCorrection[from].add(_magCorrection);
-    magnifiedFundsCorrection[to] = magnifiedFundsCorrection[to].sub(_magCorrection);
+    int256 _magCorrection = pointsPerShare.mul(value).toInt256Safe();
+    pointsCorrection[from] = pointsCorrection[from].add(_magCorrection);
+    pointsCorrection[to] = pointsCorrection[to].sub(_magCorrection);
   }
 
-  /// @dev Internal function that mints tokens to an account.
+  /**
+   * @dev Internal function that mints tokens to an account.
+   * Update pointsCorrection to keep funds unchanged.
+   * @param account The account that will receive the created tokens.
+   * @param value The amount that will be created.
+   */
   function _mint(address account, uint256 value) internal {
     super._mint(account, value);
 
-    magnifiedFundsCorrection[account] = magnifiedFundsCorrection[account]
-      .sub( (magnifiedFundsPerShare.mul(value)).toInt256Safe() );
+    pointsCorrection[account] = pointsCorrection[account]
+      .sub( (pointsPerShare.mul(value)).toInt256Safe() );
   }
   
-  /// @dev Internal function that burns an amount of the token of a given account.
+  /** 
+   * @dev Internal function that burns an amount of the token of a given account.
+   * Update pointsCorrection to keep funds unchanged.
+   * @param account The account whose tokens will be burnt.
+   * @param value The amount that will be burnt.
+   */
   function _burn(address account, uint256 value) internal {
     super._burn(account, value);
 
-    magnifiedFundsCorrection[account] = magnifiedFundsCorrection[account]
-      .add( (magnifiedFundsPerShare.mul(value)).toInt256Safe() );
+    pointsCorrection[account] = pointsCorrection[account]
+      .add( (pointsPerShare.mul(value)).toInt256Safe() );
   }
 }
